90d5576cb07e37e0b0f2d8c96901395d8849fb6c
hritik sharma <hritik2805@gmail.com>
2023-06-23 16:13:18 +0530

    register api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,2 +1,23 @@
 const catchAsyncError = require("../middleware/catchAsyncError");
 const ErrorHandler = require("../utils/errorHandler");
+const User = require("../models/userModel");
+
+exports.register = catchAsyncError(async (req, res) => {
+  const { name, email, password, avatar } = req.body;
+  console.log("Name, Email, Passord", name, email, password, avatar);
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar,
+  });
+
+  const token = user.getJWTToken();
+
+  res.status(200).send({
+    success: true,
+    message: "user is registered",
+    token,
+  });
+});
