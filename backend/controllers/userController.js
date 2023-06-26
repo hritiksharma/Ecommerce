bf4c7436de49f252c6e3257116e833f7c715d84c
hritik sharma <hritik2805@gmail.com>
2023-06-26 10:35:28 +0530

    login and register api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,3 +21,34 @@ exports.register = catchAsyncError(async (req, res) => {
     token,
   });
 });
+
+exports.login = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  console.log("Email and Password", email, password);
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Email and Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  console.log("user", user);
+
+  if (!user) {
+    return next(new ErrorHandler("Incorrect Email and password", 400));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Incorrect Email and password", 400));
+  }
+
+  const token = await user.getJWTToken();
+
+  res.status(200).send({
+    success: true,
+    message: "user is login",
+    token,
+  });
+});
