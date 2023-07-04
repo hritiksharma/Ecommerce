d07934f1e14a20a5aa85b9c3ab769fff3b185d06
hritik sharma <hritik2805@gmail.com>
2023-07-04 16:35:15 +0530

    add components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <h1>Ecommerce by hritik sharma</h1>
-    </div>
+    </>
   );
 }
 
