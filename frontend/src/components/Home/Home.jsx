90834b857334df0b9d47bad06ea2538e07fbeac1
hritiksharma <hritik2805@gmail.com>
2023-07-14 10:18:54 +0530

    changes in product page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {CgMouse} from "react-icons/cg"
-import Product from "./Product"
 import "./Home.css"
+import Product from './Product'
 
 const product = {
     name:"Blue Tshirt",
     images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
-    price:"R3000",
+    price:"₹3000",
     _id: "hritik"
 }
 const Home = () => {
@@ -22,10 +22,11 @@ const Home = () => {
     </a>
 
   </div>
-    <h2 className='homeHeading'>Features Product</h2>
 
-    <div className='container' id = "container">
-        <Product product= {product} /> 
+    <h1 className="homeHeading">Featured Products</h1>
+
+    <div className="container" id='container'>
+        <Product product={product} />
     </div>
   </>
 }
--- frontend/src/components/Home/Product.jsx
+++ frontend/src/components/Home/Product.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import ReactStars from "react-rating-stars-component"
+import "./Home.css"
 
-
-const options = {
-    edit:false,
-    color:"rgba(20,20,20,0.1",
-    activeColor: "tomato",
-    size:window.innerWidth < 600?20:25,
-    value:2.5,
-    isHalf:true,
-};
 
 const Product = ({product}) => {
+    const options =  { 
+        edit:false,
+        color:"rgba(20,20,20,0.1)",
+        activeColor:"tomato",
+        count:5,
+        value:2.5,
+        isHalf:true,
+        size:window.innerWidth < 600 ? 20 : 25,
 
-  return (  
-    <Link className='productCard' to={product._id}>
+    }
+    console.log(product);
+  return (
+    <Link className='productCard' to={product._id}> 
         <img src={product.images[0].url} alt={product.name} />
-        <p>{product.name}</p>
+        <p >{product.name} </p>
         <div> 
-            <ReactStars {...options}/> <span>256 reviews</span>
+            <ReactStars {...options} /> <span>(256 reviews)</span>
         </div>
+        <span>{product.price}</span>
     </Link>
   )
 }
