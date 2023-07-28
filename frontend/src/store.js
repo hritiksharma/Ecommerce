3e1d073cbafbe6ee5a47c0d118c4109addbf417f
Vaishnaveegupta21 <vaishu.gupta21@gmail.com>
2023-07-28 08:43:59 +0530

    changes in redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit"
-
+import { createStore, combineReducers, applyMiddleware } from "@reduxjs/toolkit"
+import thunk from "redux-thunk";
+import { } from "redux-devtools-extension"
 export const store = configureStore({
     reducer: {},
-})
+}) 
